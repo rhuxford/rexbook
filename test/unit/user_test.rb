6215ed297a1ba5e2e3487ae57cb7244646dff416
Rex Huxford <rhuxford@gmail.com>
2014-02-06 22:03:10 -0800

    Added user friendships and associated tests.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+	should have_many(:user_friendships)
+	should have_many(:friends)
+
 	test "a user should enter a first name" do
 		user = User.new
 		assert !user.save
@@ -50,5 +53,22 @@ class UserTest < ActiveSupport::TestCase
 
 		user.profile_name = 'rexhuxford_1'
 		assert user.valid?
+	end
+
+	test "that no error is raised when trying to access a friend list" do
+		assert_nothing_raised do
+			users(:rex).friends
+		end
+	end
+
+	test "that creating friendships on a user works" do
+		users(:rex).friends << users(:mike)
+		users(:rex).friends.reload
+		assert users(:rex).friends.include?(users(:mike))
+	end
+
+	test "that creating a friendship based on user id and friend id works" do
+		UserFriendship.create user_id: users(:rex).id, friend_id: users(:mike).id
+		assert users(:rex).friends.include?(users(:mike))
 	end
 end
